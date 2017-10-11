f4a3dfd0993c180598fab39c94af339060184543
voltcode <voltcode@voltcode.com>
2017-10-11 20:24:30 +0200

    initial commit

---
+++ ClassLibrary2/Class2.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public static int M2()
+        {
+            //Class1.M1();
+            //Class1.M1();
+            //Class1.M1();
+
+            return Class1.M1() + 2;
+        }
+    }
+}
---
+++ Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void IsCorrect()
+        {
+            var x = Class2.M2();
+
+            Assert.AreEqual(3, x);
+        }
+        
+    }
+}
